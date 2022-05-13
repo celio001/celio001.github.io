763dfedb878368b0ecf01c09a414a1161425edae
Célio Junior <62917562+celio001@users.noreply.github.com>
2022-05-12 22:29:55 -0300

    Add files via upload
    upload 1.0.0

---
+++ js/owl/setup.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    center: true,
+    items:2,
+    loop:true,
+    margin:10,
+    responsive:{
+        600:{
+            items:6
+        }
+    }
+});
+$('.owl-carousel').owlCarousel({
+    center: true,
+    items:2,
+    loop:false,
+    margin:10,
+    responsive:{
+        600:{
+            items:7
+        }
+    }
+});
